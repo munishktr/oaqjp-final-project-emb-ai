7ec7bedc95119eaeb23b1aa49a467bed40f4e54b
Munish Katiyar <munishktr@gmail.com>
2025-12-06 19:51:43 -0500

    app deployed for the first time

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    emotion_scores = emotion_detector(text_to_analyze)
+    emotion_text = ", ".join(
+        [f"'{emo}': {score:.9f}" for emo, score in emotion_scores.items()]
+    )
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    # Extract the formatted response
+    formatted_response = (
+        f"For the given statement, "
+        f"the system response is {emotion_text}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+
+    # Return a formatted string with the emotion score and dominant emotion
+    return formatted_response
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
